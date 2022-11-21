2b620b73ffe9c665b53ae71ae7c7dca91cc06e9d
Rumbledethumps <16963588+Rumbledethumps@users.noreply.github.com>
2022-11-20 21:13:20 -0800

    python example

--- conv.c
+++ conv.c
@@ -4,30 +4,21 @@
 
 int main()
 {
-    FILE *in, *ohex, *oarr;
+    FILE *in, *oarr;
     in = fopen("a.out", "r");
     if (NULL == in)
     {
         printf("File a.out can't be opened\n");
         exit(1);
     }
-    ohex = fopen("basic.hex", "w");
-    if (NULL == ohex)
-    {
-        printf("File basic.hex can't be written\n");
-        exit(1);
-    }
     oarr = fopen("basic.c", "w");
-    if (NULL == ohex)
+    if (NULL == oarr)
     {
         printf("File basic.c can't be written\n");
         exit(1);
     }
 
-
     int addr = fgetc(in) + (fgetc(in) << 8);
-    bool sol = true;
-
     fprintf(oarr, "char basic[] = { // $%4X\n", addr);
 
     int ch;
@@ -36,27 +27,15 @@ int main()
         ch = fgetc(in);
         if (EOF == ch)
             break;
-        if (sol)
-            fprintf(ohex, "%4X:", addr);
         fprintf(oarr, "0x%02X,", ch);
-        fprintf(ohex, " %02X", ch);
         if (++addr & 0xF)
-        {
-            sol = false;
             fprintf(oarr, " ");
-        }
         else
-        {
-            sol = true;
             fprintf(oarr, "\n");
-            fprintf(ohex, "\n");
-            if (!(addr & 0xFFF))
-                fprintf(ohex, "\n");
-        }
     }
 
     fprintf(oarr, "};\n");
-    fclose(ohex);
+    fclose(oarr);
     fclose(in);
     return 0;
 }
--- make.sh
+++ make.sh
@@ -1,3 +1,4 @@
 cc conv.c -o conv
 64tass --mw65c02 min_mon.asm
 ./conv
+python3 rp6502.py
